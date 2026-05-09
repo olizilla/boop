7d08736ad3190724901c15329c05e553b7d75b6b
Oli Evans <oli.evans@gmail.com>
2026-05-09 18:55:12 +0100

    chore: rm junk
    License: MIT
    Signed-off-by: Oli Evans <oli@zilla.org.uk>

--- src-tauri/boop-core/src/bin/check_methods.rs
+++
@@ -1,7 +0,0 @@
-use iroh::Endpoint;
-fn main() {
-    let ep: Endpoint = unimplemented!();
-    let _ = ep.connections();
-    let _ = ep.connection_infos();
-    let _ = ep.remote_info(unimplemented!());
-}
